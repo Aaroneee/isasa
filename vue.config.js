41265880d9af328ac17c18a43de70ac4974559f8
Aaron <827052236@qq.com>
2021-01-05 15:35:34 +0800

    1.更改vue打包路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    publicPath: './',   // 根路径  ====》   baseUrl: '/',已废弃
+    outputDir: 'dist', // 构建输出目录文件名打包时生成的名字。
+    assetsDir: 'assets',   // 静态资源目录（js，css，img，fonts）
+    lintOnSave: false,  // 是否开启eslint保存检测，有效值：true || false || ‘error’
 
     // webpack-dev-server 相关配置
     devServer: {
