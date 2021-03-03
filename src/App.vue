728fb47f230a5e872167e6be5ea6ee19d270ecdc
BenOniShi <shibn9510@gmail.com>
2021-03-03 17:36:14 +0800

    2021-03-03 Vue provide 结合 router KeepAlive

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <router-view v-if="$route.meta.keepAlive"></router-view>
-    </keep-alive>
-    <router-view v-if="!$route.meta.keepAlive"></router-view>
+    <div v-if="isRouterAlive">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive">
+      </router-view>
+    </div>
   </div>
 </template>
-
 <script>
-
 
 export default {
   name: 'App',
-
+  provide() {
+    return {
+      reload: this.reload
+    }
+  },
+  data() {
+    return {
+      isRouterAlive: true
+    }
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false
+      this.$nextTick(function () {
+        this.isRouterAlive = true
+      })
+    }
+  },
+  components: {}
 }
 </script>
 
