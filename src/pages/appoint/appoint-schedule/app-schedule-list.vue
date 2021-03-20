53375b7b6352513d76c1adc79aeffdf135ff3bd7
BenOniShi <shibn9510@gmail.com>
2021-03-20 19:01:15 +0800

    2021-03-20 预约档期 款式档期

---
+++ src/pages/appoint/appoint-schedule/app-schedule-list.vue
@@ -0,0 +1,172 @@
+<template>
+  <div>
+    <van-sticky>
+      <switchNavBar title="每日预约" switchText="日期" @flag="createDateShow=true"/>
+      <van-search
+          @search="queryAppList"
+          v-model="searchValue"
+          placeholder="请输入搜索关键词"/>
+      <van-calendar  v-model="createDateShow" :min-date="new Date('2020/01/01')"
+                    :max-date="new Date('2022/01/01')"  @confirm="createDateOnConfirm"/>
+      <van-dropdown-menu>
+        <van-dropdown-item v-model="appointName" @change="appointNameChange"
+                           :options="appointNameArray"/>
+        <van-dropdown-item v-model="inviter" @change="inviterChange" :options="inviterArray"/>
+        <van-dropdown-item v-model="appointDress" @change="appointDressChange"
+                           :options="appointDressArray"/>
+        <van-dropdown-item v-model="appointShop" @change="appointShopChange"
+                           :options="appointShopArray"/>
+      </van-dropdown-menu>
+    </van-sticky>
+    <div>
+      <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+      >
+        <van-cell v-for="item in appointList" :key="item.id" @click="clickItem(item.id)">
+          <van-row style="padding-bottom: 10px">
+            <van-col span="12">姓名:{{ item.name }}</van-col>
+            <van-col v-if="item.isValid === '1'" style="color: coral">状态 : 预约</van-col>
+            <van-col v-if="item.isValid === '2'" style="color: #39a9ed">状态 : 到店</van-col>
+            <van-col v-if="item.isValid ==='3'" style="color: red">状态 : 取消</van-col>
+          </van-row>
+          <van-row>
+            <van-col span="12">预约日期:{{ item.createDate }}</van-col>
+            <van-col span="12">到店日期:{{ item.appointDate }}</van-col>
+          </van-row>
+          <van-row>
+            <van-col span="12">来源:{{ item.sourceName }}</van-col>
+            <van-col span="12">到店时间:{{ item.appointTime }}</van-col>
+          </van-row>
+          <van-row>
+            <van-col span="12">预约项目:{{ item.appointName }}</van-col>
+            <van-col span="12">城市:{{ item.appointCity }}</van-col>
+          </van-row>
+          <van-row>
+            <van-col span="12">预约人:{{ item.inviter }}</van-col>
+            <van-col span="12">店铺:{{ item.appointShop }}</van-col>
+          </van-row>
+          <van-row>
+            <van-col span="24">备注:{{ item.appointRemark }}</van-col>
+          </van-row>
+        </van-cell>
+      </van-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import switchNavBar from '@/components/nav-bar/switch-nav-bar'
+
+export default {
+  name: "appoint-schedule-list",
+  data() {
+    return {
+      tenantCrop: localStorage.getItem("tenantCrop"),
+      appointList: [],
+      searchValue: "",
+      loading: false,
+      finished: false,
+
+      createDateShow: false,
+      appointNameArray: [{text: "项目", value: ""}],
+      inviterArray: [{text: "预约人", value: ""}],
+      appointDressArray: [{text: "礼服师", value: ""}],
+      appointShopArray: [{text: "店铺", value: ""}],
+
+      appointDate: this.$dateUtils.vantDateToYMD(new Date()),
+      appointName: "",
+      inviter: "",
+      appointDress: "",
+      appointShop: "",
+      state: "",
+    }
+  },
+  components: {
+    switchNavBar: switchNavBar,
+  },
+  created() {
+    this.queryAppList();
+    this.queryAppointName();
+    this.queryInviter();
+    this.queryAppointDress();
+    this.queryAppointShop();
+  },
+  methods: {
+    //日历确认
+    createDateOnConfirm: function (time) {
+      this.appointDate = this.$dateUtils.vantDateToYMD(time);
+      this.queryAppList();
+      this.createDateShow = false;
+    },
+    inviterChange: function (value) {
+      this.inviter = value;
+      this.queryAppList();
+    },
+    appointNameChange: function (value) {
+      this.appointName = value;
+      this.queryAppList();
+    },
+    appointDressChange: function (value) {
+      this.appointDress = value;
+      this.queryAppList();
+    },
+    appointShopChange: function (value) {
+      this.appointShop = value;
+      this.queryAppList();
+    },
+    clickItem: function (id) {
+      this.$router.push({name: "appDetails", query: {id: id}})
+    },
+
+
+    queryAppList: function () {
+      this.$axios({
+        method: "GET",
+        url: "/appoint/mAppointList",
+        params: {
+          value: this.searchValue,
+          appointDate: this.appointDate,
+          appointName: this.appointName,
+          inviter: this.inviter,
+          appointDress: this.appointDress,
+          appointShop: this.appointShop,
+          tenantCrop: this.tenantCrop
+        }
+      }).then(response => {
+        this.appointList = response.data.data.list;
+        this.finished = true;
+      })
+    },
+    //查询预约项目
+    queryAppointName: function () {
+      this.$selectUtils.queryProjectsIds(this.$projectsType.appoint, this.$selectUtils.DropDownMenu).then(response => {
+        this.appointNameArray.push(...JSON.parse(response.data.data));
+      })
+    },
+    //查询预约人
+    queryInviter: function () {
+      this.$selectUtils.queryEmpIds(this.$selectUtils.DropDownMenu).then(response => {
+        this.inviterArray.push(...JSON.parse(response.data.data));
+      })
+    },
+    //查询礼服师
+    queryAppointDress: function () {
+      this.$selectUtils.queryDressIds(this.$selectUtils.DropDownMenu).then(response => {
+        this.appointDressArray.push(...JSON.parse(response.data.data));
+      })
+    },
+    //查询店铺
+    queryAppointShop: function () {
+      this.$selectUtils.queryShopIds(this.$selectUtils.DropDownMenu).then(response => {
+        this.appointShopArray.push(...JSON.parse(response.data.data));
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/clothes/schedule/schedule-list.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+
+    <van-sticky>
+      <switchNavBar title="档期查询" :switchText="dateText" @flag="dateShow=true"/>
+      <van-search
+          @search="queryClothesList"
+          v-model="searchValue"
+          placeholder="请输入婚纱编号"/>
+      <van-calendar safe-area-inset-bottom v-model="dateShow" :min-date="new Date('2020/01/01')" :max-date="new Date('2022/01/01')"  @confirm="dateOnConfirm"/>
+    </van-sticky>
+  </div>
+
+</template>
+
+<script>
+import switchNavBar from "@/components/nav-bar/switch-nav-bar"
+
+export default {
+  name: "schedule-list"
+  , components: {
+    switchNavBar: switchNavBar,
+  },
+  data(){
+    return{
+      searchValue:"",
+      dateShow:false,
+      scheduleDate:"",
+      dateText:"选择档期"
+    }
+  }
+  ,methods:{
+    queryClothesList:function (value){
+      console.log(value)
+    },dateOnConfirm:function (value){
+      const s = this.$dateUtils.vantDateToYMD(value);
+      this.scheduleDate = s
+      this.dateText = s
+      this.dateShow = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
