8e25ae7494c8e1576b162a848fce729403c43174
BenOniShi <shibn9510@gmail.com>
2021-03-29 18:22:18 +0800

    2021-03-29 app-add 格式化

--- src/pages/appoint/app-add.vue
+++ src/pages/appoint/app-add.vue
@@ -30,7 +30,8 @@
           @click="createDateShowPicker = true"
           :rules="[{ required: true }]"
       />
-      <van-calendar v-model="createDateShowPicker" :min-date="new Date('2020/01/01')" :max-date="new Date('2022/01/01')" @confirm="createDateOnConfirm"/>
+      <van-calendar v-model="createDateShowPicker" :min-date="new Date('2020/01/01')" :max-date="new Date('2022/01/01')"
+                    @confirm="createDateOnConfirm"/>
 
       <van-field
           readonly
@@ -42,7 +43,8 @@
           @click="appointDateShowPicker = true"
           :rules="[{ required: true }]"
       />
-      <van-calendar v-model="appointDateShowPicker" :min-date="new Date('2020/01/01')" :max-date="new Date('2022/01/01')" @confirm="appointDateOnConfirm"/>
+      <van-calendar v-model="appointDateShowPicker" :min-date="new Date('2020/01/01')"
+                    :max-date="new Date('2022/01/01')" @confirm="appointDateOnConfirm"/>
 
       <van-field
           name="appointTime"
@@ -156,114 +158,112 @@ import baseNavBar from '@/components/nav-bar/base-nav-bar'
 
 export default {
   name: "app_add",
-  data(){
-    return{
-      customer:{},
-      createDate:this.$dateUtils.vantDateToYMD(new Date()),
-      appointDate:"",
-      appointTime:"",
+  data() {
+    return {
+      customer: {},
+      createDate: this.$dateUtils.vantDateToYMD(new Date()),
+      appointDate: "",
+      appointTime: "",
 
-      inviter:"",
-      inviterText:"",
-      inviterArray:[],
+      inviter: "",
+      inviterText: "",
+      inviterArray: [],
 
-      appointName:"",
-      appointNameText:"",
-      appointNameArray:[],
+      appointName: "",
+      appointNameText: "",
+      appointNameArray: [],
 
-      appointShop:"",
-      appointShopText:"",
-      appointShopArray:[],
+      appointShop: "",
+      appointShopText: "",
+      appointShopArray: [],
 
-      appointCity:"",
+      appointCity: "",
 
-      appointRemark:"",
+      appointRemark: "",
 
 
-      createDateShowPicker:false,
-      appointDateShowPicker:false,
-      appointTimeShowPicker:false,
-      appointTimeColumns:appointTime,
-      inviterShowPicker:false,
-      appointNameShowPicker:false,
-      appointShopShowPicker:false,
-      tenantCrop:localStorage.getItem("tenantCrop"),
+      createDateShowPicker: false,
+      appointDateShowPicker: false,
+      appointTimeShowPicker: false,
+      appointTimeColumns: appointTime,
+      inviterShowPicker: false,
+      appointNameShowPicker: false,
+      appointShopShowPicker: false,
+      tenantCrop: localStorage.getItem("tenantCrop"),
     }
   },
   components: {
     baseNavBar: baseNavBar
   },
   created() {
-    this.customer=this.$route.query;
+    this.customer = this.$route.query;
     this.queryInviterIds();
     this.queryProjectsIds();
     this.queryShopIds();
 
   },
-  methods:{
+  methods: {
     //预约日期
-    createDateOnConfirm:function (time){
+    createDateOnConfirm: function (time) {
       this.createDate = this.$dateUtils.vantDateToYMD(time);
       this.createDateShowPicker = false;
 
     },
     //到店日期
-    appointDateOnConfirm:function (time){
+    appointDateOnConfirm: function (time) {
       this.appointDate = this.$dateUtils.vantDateToYMD(time);
       this.appointDateShowPicker = false;
     },
     //到店时间
-    appointTimeOnConfirm:function (value){
-      this.appointTime=value.text;
-      this.appointTimeShowPicker=false;
+    appointTimeOnConfirm: function (value) {
+      this.appointTime = value.text;
+      this.appointTimeShowPicker = false;
     },
     //预约人
-    inviterOnConfirm:function (value){
-      this.inviterText=value.text;
-      this.inviter=value.id;
-      this.inviterShowPicker=false;
+    inviterOnConfirm: function (value) {
+      this.inviterText = value.text;
+      this.inviter = value.id;
+      this.inviterShowPicker = false;
     },
     //预约项目
-    appointNameOnConfirm:function (value){
-      this.appointNameText=value.text;
-      this.appointName=value.id;
-      this.appointNameShowPicker=false;
+    appointNameOnConfirm: function (value) {
+      this.appointNameText = value.text;
+      this.appointName = value.id;
+      this.appointNameShowPicker = false;
     },
     //查询店铺
-    appointShopOnConfirm:function (value){
-      this.appointShopText=value.text;
-      this.appointShop=value.id;
-      this.appointShopShowPicker=false;
+    appointShopOnConfirm: function (value) {
+      this.appointShopText = value.text;
+      this.appointShop = value.id;
+      this.appointShopShowPicker = false;
       //查询店铺所在的城市
       this.queryCityByShopIds(value.id);
     },
 
 
-    addAppointSubmit:function (values){
-      values.cusId=this.customer.id;
-      values.inviter=this.inviter;
-      values.appointName=this.appointName;
-      values.appointShop=this.appointShop;
-      values.type="售前预约";
+    addAppointSubmit: function (values) {
+      values.cusId = this.customer.id;
+      values.inviter = this.inviter;
+      values.appointName = this.appointName;
+      values.appointShop = this.appointShop;
+      values.type = "售前预约";
       values.tenantCrop = this.tenantCrop;
       this.$dialog.confirm({
         title: '添加预约',
         message: '是否确认给该条客资添加预约?',
       }).then(() => {
         this.$axios({
-          method:"POST",
-          url:"/appoint/saveAppoint",
-          params:values
-        }).then(response=>{
-
-          response.data.code===200?this.$toast.success("添加成功"):this.$toast.fail(response.data.msg);
-          if (response.data.code===200){
+          method: "POST",
+          url: "/appoint/saveAppoint",
+          params: values
+        }).then(response => {
+          if (response.data.code === 200) {
             this.$toast.success("添加预约成功")
             const that = this
             setTimeout(function () {
               that.$router.back()
             }, 1000)
-          }else {
+          } else {
             this.$toast.fail(response.data.code);
           }
         })
@@ -271,64 +271,63 @@ export default {
     },
 
 
-
     //查询预约人
-    queryInviterIds:function (){
-      this.$selectUtils.queryEmpIds(this.$selectUtils.Picker).then(response=>{
-        this.inviterArray=JSON.parse(response.data.data);
+    queryInviterIds: function () {
+      this.$selectUtils.queryEmpIds(this.$selectUtils.Picker).then(response => {
+        this.inviterArray = JSON.parse(response.data.data);
       })
     },
     //查询预约项目
-    queryProjectsIds:function (){
-      this.$selectUtils.queryProjectsIds(this.$projectsType.appoint,this.$selectUtils.Picker).then(response=>{
-        this.appointNameArray=JSON.parse(response.data.data);
+    queryProjectsIds: function () {
+      this.$selectUtils.queryProjectsIds(this.$projectsType.appoint, this.$selectUtils.Picker).then(response => {
+        this.appointNameArray = JSON.parse(response.data.data);
       })
     },
     //查询店铺
-    queryShopIds:function (){
-      this.$selectUtils.queryShopIds(this.$selectUtils.Picker).then(response=>{
-        this.appointShopArray=JSON.parse(response.data.data);
+    queryShopIds: function () {
+      this.$selectUtils.queryShopIds(this.$selectUtils.Picker).then(response => {
+        this.appointShopArray = JSON.parse(response.data.data);
       })
     },
     //根据店铺查询城市
-    queryCityByShopIds:function (id){
-      this.$selectUtils.queryCityByShopIds(id).then(response=>{
-        this.appointCity= response.data.msg;
+    queryCityByShopIds: function (id) {
+      this.$selectUtils.queryCityByShopIds(id).then(response => {
+        this.appointCity = response.data.msg;
       })
     },
   }
 
 }
-const appointTime=[
-  {text:"06:00",id:"06:00"},
-  {text:"07:00",id:"07:00"},
-  {text:"08:00",id:"08:00"},
-  {text:"08:30",id:"08:30"},
-  {text:"09:00",id:"09:00"},
-  {text:"09:30",id:"09:30"},
-  {text:"10:00",id:"10:00"},
-  {text:"10:30",id:"10:30"},
-  {text:"11:00",id:"11:00"},
-  {text:"11:30",id:"11:30"},
-  {text:"12:00",id:"12:00"},
-  {text:"12:30",id:"12:30"},
-  {text:"13:00",id:"13:00"},
-  {text:"13:30",id:"13:30"},
-  {text:"14:00",id:"14:00"},
-  {text:"14:30",id:"14:30"},
-  {text:"15:00",id:"15:00"},
-  {text:"15:30",id:"15:30"},
-  {text:"16:00",id:"16:00"},
-  {text:"16:30",id:"16:30"},
-  {text:"17:00",id:"17:00"},
-  {text:"17:30",id:"17:30"},
-  {text:"18:00",id:"18:00"},
-  {text:"18:30",id:"18:30"},
-  {text:"19:00",id:"19:00"},
-  {text:"19:30",id:"19:30"},
-  {text:"20:00",id:"20:00"},
-  {text:"20:30",id:"20:30"},
-  {text:"21:00",id:"21:00"},
+const appointTime = [
+  {text: "06:00", id: "06:00"},
+  {text: "07:00", id: "07:00"},
+  {text: "08:00", id: "08:00"},
+  {text: "08:30", id: "08:30"},
+  {text: "09:00", id: "09:00"},
+  {text: "09:30", id: "09:30"},
+  {text: "10:00", id: "10:00"},
+  {text: "10:30", id: "10:30"},
+  {text: "11:00", id: "11:00"},
+  {text: "11:30", id: "11:30"},
+  {text: "12:00", id: "12:00"},
+  {text: "12:30", id: "12:30"},
+  {text: "13:00", id: "13:00"},
+  {text: "13:30", id: "13:30"},
+  {text: "14:00", id: "14:00"},
+  {text: "14:30", id: "14:30"},
+  {text: "15:00", id: "15:00"},
+  {text: "15:30", id: "15:30"},
+  {text: "16:00", id: "16:00"},
+  {text: "16:30", id: "16:30"},
+  {text: "17:00", id: "17:00"},
+  {text: "17:30", id: "17:30"},
+  {text: "18:00", id: "18:00"},
+  {text: "18:30", id: "18:30"},
+  {text: "19:00", id: "19:00"},
+  {text: "19:30", id: "19:30"},
+  {text: "20:00", id: "20:00"},
+  {text: "20:30", id: "20:30"},
+  {text: "21:00", id: "21:00"},
 ]
 </script>
 
