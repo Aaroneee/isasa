e278312a6a48c233246798637e24eeea8cd32823
BenOniShi <shibn9510@gmail.com>
2021-05-16 18:28:23 +0800

    2021-05-16 配置文件不要随便修改 修改后记得改回来

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ Vue.prototype.$projectsType = projectsType;
 Vue.use(SlimCropper)
 Vue.config.productionTip = false
 
-// axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = '/api'
 export default new Vue({
     router,
     render: h => h(App),
