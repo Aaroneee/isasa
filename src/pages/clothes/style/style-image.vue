088ccdf69775050384d4671ef39b0784f4f93286
chentg <728246363@qq.com>
2022-03-16 18:09:58 +0800

    订单图片小图质量为60%

--- src/pages/clothes/style/style-image.vue
+++ src/pages/clothes/style/style-image.vue
@@ -15,7 +15,7 @@
           <van-image
               lazy-load
               style="width: 100%;height: 100%"
-              :src="'\thttps://clothes-image-1304365928.cos.ap-shanghai.myqcloud.com/'+chil.styleImage"
+              :src="'\thttps://clothes-image-1304365928.cos.ap-shanghai.myqcloud.com/'+chil.styleImage+'?imageMogr2/rquality/60'"
               @click="imageShowClick(item, index)">
             <template v-slot:loading>
               <van-loading type="spinner" size="20" />
