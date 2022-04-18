2d5229a4fd7ed819942314bed1646e82c88d8b94
‘Aaroneee’ <827052236@qq.com>
2022-04-18 14:47:12 +0800

    1.婚纱列表Pad版 搜索记录保存

--- src/pages/clothes/clothes/clothes-list-pad.vue
+++ src/pages/clothes/clothes/clothes-list-pad.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <van-sticky>
-      <baseNavBar title="订单档期查询"/>
+      <switchNavBar title="订单档期查询" :switchText="'常用条件'" @flag="recordsShow=true"/>
       <form action="javascript:return true">
         <van-search
             show-action
@@ -9,7 +9,9 @@
             v-model="styleName"
             placeholder="请输入婚纱礼服名称">
           <template #action>
-            <div @click="codeScanShow=!codeScanShow"><van-icon name="scan" /></div>
+            <div @click="codeScanShow=!codeScanShow">
+              <van-icon name="scan"/>
+            </div>
           </template>
         </van-search>
       </form>
@@ -57,12 +59,13 @@
       >
         <van-cell style="font-size: 12px">
           <van-row gutter="5">
-            <van-col span="6"  v-for="item in clothesList" :key="item.id" @click="clickItem(item)" style="text-align: center">
+            <van-col span="6" v-for="item in clothesList" :key="item.id" @click="clickItem(item)"
+                     style="text-align: center">
               <div class="card">
                 <div class="imgFont">
-                  <van-image  class="style-img" radius="7"
-                              fit="contain"
-                              :src="item.styleImage===''?'null'
+                  <van-image class="style-img" radius="7"
+                             fit="contain"
+                             :src="item.styleImage===''?'null'
                              :'https://clothes-image-1304365928.cos.ap-shanghai.myqcloud.com/'+item.styleImage+'?imageMogr2/rquality/60'">
                     <template v-slot:error>加载失败,请更换主图</template>
                   </van-image>
@@ -70,7 +73,8 @@
 
                     <van-row>
                       <van-col span="14">
-                        <p class="pFont" style="text-align: left">{{item.styleType+'-'+item.styleName+'-'+item.clothesSize+'-'+item.clothesNo}}</p>
+                        <p class="pFont" style="text-align: left">
+                          {{ item.styleType + '-' + item.styleName + '-' + item.clothesSize + '-' + item.clothesNo }}</p>
                       </van-col>
                       <van-col span="10">
                         <p class="pFont" style="text-align: right">{{ item.styleAlias }}</p>
@@ -78,7 +82,7 @@
                     </van-row>
                     <van-row>
                       <van-col span="14">
-                        <p class="pFont" style="text-align: left">{{item.brand===''?'暂无品牌':item.brand}}</p>
+                        <p class="pFont" style="text-align: left">{{ item.brand === '' ? '暂无品牌' : item.brand }}</p>
                       </van-col>
                       <van-col span="10">
                         <p class="pFont" style="text-align: right">{{ item.positionName }}</p>
@@ -93,6 +97,49 @@
         </van-cell>
       </van-list>
     </div>
+    <!--    recordsPopup-->
+
+
+    <van-popup v-model="recordsShow" position="right" :style="{ width:'20%' }">
+      <van-row style="text-align: center">
+        <van-button style="width: 100%" plain type="primary" @click="saveRecord()">保存当前条件</van-button>
+      </van-row>
+      <div class="popup-card" v-for="(item,index) in recordsList" :key="index">
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">款式类型</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.styleTypeString }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">婚纱尺寸</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.clothesSize }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">款式标签</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.styleLabelsString }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">款式品牌</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.brandName }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">店铺</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.shopName }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row">
+          <van-col :span="10" :offset="1"><span style="text-align: left;font-size: 15px">位置</span></van-col>
+          <van-col :span="12"><span style="text-align: right;font-size: 15px">{{ item.positionName }}</span></van-col>
+        </van-row>
+        <van-row class="popup-card-row" style="text-align: center">
+          <van-col :span="12">
+            <van-button plain  type="info" @click="useRecord(item)">使用此条件</van-button>
+          </van-col>
+          <van-col :span="12">
+            <van-button plain  type="danger" @click="delRecord(item.id)">删除此条件</van-button>
+          </van-col>
+        </van-row>
+
+      </div>
+    </van-popup>
   </div>
 </template>
 
@@ -109,17 +156,19 @@ export default {
     this.queryStyleLabelList()
     this.queryClothesBrand()
     this.queryClothesSize()
+    this.queryRecordList()
   },
   data() {
     return {
       codeScanShow: false,
-      dateShow:false,
-      dateText:"选择档期",
-      scheduleDate:"",
+      dateShow: false,
+      dateText: "选择档期",
+      scheduleDate: "",
       tenantCrop: localStorage.getItem("tenantCrop"),
+      empId: localStorage.getItem("empId"),
 
-      maxDate:this.$dateUtils.getMaxMinDate()[0],
-      minDate:this.$dateUtils.getMaxMinDate()[1],
+      maxDate: this.$dateUtils.getMaxMinDate()[0],
+      minDate: this.$dateUtils.getMaxMinDate()[1],
 
       styleName: "",
       styleType: "",
@@ -136,19 +185,22 @@ export default {
       positionArray: [],
       styleLabelList: [],
       styleLabels: [],
-      isOrder:"",
-      isOrderArray:[{text: "全部", value: ""},{text: "有档期不可用", value: "isOrder"},{text: "无档期可用", value: "notOrder"}],
+      isOrder: "",
+      isOrderArray: [{text: "全部", value: ""}, {text: "有档期不可用", value: "isOrder"}, {text: "无档期可用", value: "notOrder"}],
+      //搜索记录
+      recordsShow: false,
+      recordsList: [],
 
       isactive: false,
       page: 1,
 
-      brandText:"",
-      brand:"",
+      brandText: "",
+      brand: "",
       styleBrandArray: [{text: "品牌", value: ""}],
     }
   },
   components: {
-    switchNavBar,baseNavBar
+    switchNavBar, baseNavBar
   },
   watch: {
     codeScanShow: function (newVal) {
@@ -159,8 +211,6 @@ export default {
   },
   methods: {
     clickItem: function (value) {
-      console.log(value)
-      // TODO  婚纱详情待开发
       this.$router.push({name: "clothesDetailsPad", query: value})
     }, searchStyleName: function (value) {
       this.page = 1
@@ -176,7 +226,7 @@ export default {
         url: '/clothes/mClothesList',
         params: {
           page: this.page,
-          limit:40,
+          limit: 40,
           styleLabels: this.styleLabels.toString(),
           styleName: this.styleName,
           styleType: this.styleType,
@@ -184,8 +234,8 @@ export default {
           tenantCrop: this.tenantCrop,
           clothesShop: this.shop,
           clothesPosition: this.position,
-          scheduleDate:this.scheduleDate,
-          isOrder:this.isOrder,
+          scheduleDate: this.scheduleDate,
+          isOrder: this.isOrder,
           brandName: this.brand,
         }
       }).then(response => {
@@ -197,20 +247,18 @@ export default {
             this.page = response.data.data.nextPage
           }
           this.clothesList.push(...response.data.data.list)
-          console.log(this.clothesList)
         } else {
           this.finished = true
           this.$toast.fail(response.data.msg);
         }
       })
-    }
-    , onLoad() {
+    },
+    onLoad() {
       const that = this
       setTimeout(function () {
         that.queryClothesList()
       }, 1000)
-    }
-    ,
+    },
     styleTypeChange: function (type) {
       this.flushClothesListArray()
       this.styleType = type
@@ -219,8 +267,8 @@ export default {
     shopChange: function (shop) {
       this.flushClothesListArray()
       this.shop = shop
-      this.position="";
-      this.positionTitle="位置";
+      this.position = "";
+      this.positionTitle = "位置";
       this.queryPositionIdsByShop()
       this.queryClothesList()
     },
@@ -248,7 +296,7 @@ export default {
     },
     queryPositionIdsByShop() {
       this.$selectUtils.queryPositionIdsByShop(this.shop, this.$selectUtils.DropDownMenu).then(response => {
-        this.positionArray=JSON.parse(response.data.data)
+        this.positionArray = JSON.parse(response.data.data)
       })
     },
     flushClothesListArray: function () {
@@ -256,14 +304,9 @@ export default {
       this.page = 1
       this.finished = false
     },
-    toScan: function () {
-      // TODO
-      // 消息通知到 原生 原生隐藏vue页面   拉起扫码  扫码结束传递 值
-    },
     queryStyleLabelList: function () {
       this.$selectUtils.queryStyleLabels().then((response) => {
         this.styleLabelList.push(...response.data.data);
-        console.log(response)
       })
     },
     styleLabelConfirm: function () {
@@ -279,26 +322,26 @@ export default {
         this.styleLabels.push(value)
       }
     },
-    dateOnConfirm:function (value){
+    dateOnConfirm: function (value) {
       this.isOrder = "notOrder";
       const s = this.$dateUtils.rangeVantDateToYMD(value);
       this.scheduleDate = s
       const temp = s.split('-')
       this.dateText = temp[1] + "-" + temp[2] + " - " + temp[4] + "-" + temp[5]
       this.dateShow = false
-      if (this.isOrder === ""){
+      if (this.isOrder === "") {
         this.isOrder = "isOrder"
       }
       this.flushClothesListArray()
       this.queryClothesList()
     },
-    isOrderChange:function (val){
+    isOrderChange: function (val) {
       //如果选择的全部 则删除档期时间
-      if (val===''){
-        this.scheduleDate="";
-        this.dateText="选择档期";
+      if (val === '') {
+        this.scheduleDate = "";
+        this.dateText = "选择档期";
       }
-      if (this.scheduleDate === ""){
+      if (this.scheduleDate === "") {
         this.$toast.fail("选择档期后该选项生效")
         return
       }
@@ -320,6 +363,96 @@ export default {
         for (let temp of data) {
           this.clothesSizeArray.push({text: temp, value: temp})
         }
+      })
+    },
+    //查询条件记录
+    queryRecordList() {
+      this.$axios({
+        method: "GET",
+        url: '/record/queryRecordList',
+        params: {
+          tenantCrop: this.tenantCrop,
+          empId: this.empId,
+          type: "婚纱列表",
+        }
+      }).then(response => {
+        this.recordsList = response.data.data;
+        console.log(response)
+      })
+    },
+    //保存当前搜索条件
+    saveRecord() {
+      //查找款式类型String
+      let styleTypeString = this.styleType === '' ? "" : this.styleTypeArray.filter(v => v.value === this.styleType)[0].text;
+
+      //查找款式标签String
+      let styleLabelsString = [];
+      if (this.styleLabels.length !== 0) {
+        let labelIds = this.styleLabels.toString().split(",");
+        labelIds.forEach(la => {styleLabelsString.push(this.styleLabelList.filter(v => v.value === Number(la))[0].name)})
+      }
+      //查找店铺String
+      let shopName = this.shop === ""?"":this.shopArray.filter(v => v.value === this.shop)[0].text;
+
+      //查找位置String
+      let positionName = this.position === ''?"":positionName = this.positionArray.filter(v => v.value === this.position)[0].text;;
+
+      this.$axios({
+        method: "POST",
+        url: '/record/addRecordOfCustomer',
+        params: {
+          styleType:this.styleType,
+          styleTypeString:styleTypeString,
+          styleLabels:this.styleLabels.toString(),
+          styleLabelsString:styleLabelsString.toString(),
+          shopId:this.shop,
+          shopName:shopName,
+          positionId:this.position,
+          positionName:positionName,
+          clothesSize:this.clothesSize,
+          brandName:this.brand,
+          tenantCrop:this.tenantCrop,
+          empId:this.empId,
+          recordType:"婚纱列表",
+        }
+      }).then(response => {
+        response.data.code === 200?this.$toast.success("添加成功"):this.$toast.fail(response.data.msg)
+        this.queryRecordList();
+      })
+    },
+    useRecord(item){
+      console.log(item)
+      if (item.styleType!==""){this.styleType=item.styleType}
+      if (item.clothesSize!==""){this.clothesSize=item.clothesSize}
+
+      if (item.styleLabels!==""){
+        let labelIds=item.styleLabels.split(",");
+        labelIds.forEach(k=>this.pushStyleLabel(Number(k)))
+      }
+      if (item.brandName!==""){this.brand=item.brandName}
+
+      if (item.shopId!==""){
+        this.shop=item.shopId;
+        this.queryPositionIdsByShop();
+      }
+      if (item.positionId!==""){this.position=item.positionId}
+      this.flushClothesListArray();
+      this.queryClothesList();
+    },
+    delRecord(id) {
+      this.$dialog.confirm({
+        title: "是否确认删除?",
+      }).then(() => {
+        this.$axios({
+          method: "POST",
+          url: "/record/deleteById",
+          params: {
+            id: id
+          }
+        }).then((response) => {
+          response.data.code === 200 ? this.$toast.success("删除成功") : this.$toast.fail(response.data.msg)
+          this.queryRecordList();
+        })
       })
     },
   },
@@ -352,6 +485,7 @@ export default {
   max-width: 165px;
   display: block;
 }
+
 /*p{*/
 /*  overflow: hidden;*/
 /*  white-space: nowrap;*/
@@ -362,25 +496,29 @@ export default {
   margin: 0 auto;
   width: 100%;
 }
-.card{
+
+.card {
   min-height: 265px;
   max-height: 265px;
   min-width: 160px;
-  padding: 5px 10px 0 10px ;
+  padding: 5px 10px 0 10px;
   background-color: white;
   border-radius: 10px;
   margin: 0 auto 3% auto;
 }
-.imgFont{
+
+.imgFont {
   margin: 0 auto;
   width: 100%;
   min-width: 165px;
   max-width: 165px;
 }
-.styleInfo{
+
+.styleInfo {
   margin-top: 5px;
 }
-.pFont{
+
+.pFont {
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
@@ -391,19 +529,36 @@ export default {
   /*margin-block-start: 5px;*/
   /*margin-block-end: 5px;*/
 }
-.van-list >>> .van-cell{
+
+.van-list >>> .van-cell {
   padding: 10px 5px;
-  background-color:#f7f8fa;
-  line-height:17px;
+  background-color: #f7f8fa;
+  line-height: 17px;
   /*background-color: #1a2a4c;*/
 }
-.card >>> .van-badge--fixed{
+
+.card >>> .van-badge--fixed {
   left: 0;
   right: auto;
   -webkit-transform: none;
 }
+
 .bgcolor {
   border: 1px solid #de0d0d;
   color: rgb(182, 177, 189);
+}
+
+.van-popup {
+  background-color: unset;
+}
+.popup-card {
+  margin-top: 15px;
+  border-radius: 15px 15px;
+  background-color: #e1e1e1;
+}
+
+.popup-card-row {
+  padding: 10px 10px;
+  color: black;
 }
 </style>
