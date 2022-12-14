7226757621c6854713f856c0249110e1882446b4
jht <1751263383@qq.com>
2022-12-14 14:55:10 +0800

    完成接待详情、订单详情报表

--- src/pages/statement/shop/shop-daily-reports.vue
+++ src/pages/statement/shop/shop-daily-reports.vue
@@ -70,33 +70,33 @@
       </van-collapse-item>
       <van-collapse-item title="营收报告" name="2" icon="shop-o">
         <van-grid :border="false" style="text-align: center" :column-num="4">
-          <van-grid-item v-for="value in revenueReports" :key="value.projectName" v-if="value.sumMoney!= 0" @click="showProjectsDetails(value.projectName)">
+          <van-grid-item v-for="value in proceedsAnalysis" :key="value.projectName" v-if="value.sumMoney!= 0" @click="showProjectsDetails(value.projectName)">
             {{value.projectName}}<br>{{value.sumMoney}}
           </van-grid-item>
         </van-grid>
       </van-collapse-item>
-      <van-collapse-item title="押金报告" name="3" icon="shop-o">
+<!--      <van-collapse-item title="押金报告" name="3" icon="shop-o">
         <van-grid :border="false" style="text-align: center" :column-num="4">
           <van-grid-item v-for="value in depositReports" :key="value.projectName" v-if="value.sumMoney!= 0" @click="showProjectsDetails(value.projectName)">
             {{value.projectName}}<br>{{value.sumMoney}}
           </van-grid-item>
         </van-grid>
-      </van-collapse-item>
+      </van-collapse-item>-->
       <van-collapse-item title="售前情况" name="4" icon="shop-o">
         <van-grid :border="false" style="text-align: center" :column-num="3">
-          <van-grid-item>售前接待数<br>{{saleReception}}</van-grid-item>
-          <van-grid-item>一次订单数<br>{{onceTheOrder}}</van-grid-item>
-          <van-grid-item>一次未订单数<br>{{onceNotOrder}}</van-grid-item>
-          <van-grid-item>二次订单数<br>{{twiceTheOrder}}</van-grid-item>
+          <van-grid-item @click="showAppointSaleDetails('售前接待')">售前接待数<br>{{saleReception}}</van-grid-item>
+          <van-grid-item @click="showOrderDetails('一次订单')">一次订单数<br>{{onceTheOrder}}</van-grid-item>
+          <van-grid-item @click="showAppointDetails('一次未订单')">一次未订单数<br>{{onceNotOrder}}</van-grid-item>
+          <van-grid-item @click="showOrderDetails('二次订单')">二次订单数<br>{{twiceTheOrder}}</van-grid-item>
           <van-grid-item>一次转换率<br>{{onceProportion}}</van-grid-item>
           <van-grid-item>综合转换率<br>{{comprehensiveProportion}}</van-grid-item>
         </van-grid>
       </van-collapse-item>
       <van-collapse-item title="售后情况" name="5" icon="shop-o">
         <van-grid :border="false" style="text-align: center" :column-num="3">
-          <van-grid-item>售后接待数<br>{{afterSaleReception}}</van-grid-item>
-          <van-grid-item>售后升级接待数<br>{{afterSaleUpGradeReception}}</van-grid-item>
-          <van-grid-item>售后订单数<br>{{afterSaleOrderNum}}</van-grid-item>
+          <van-grid-item @click="showAppointSaleDetails('售后接待')">售后接待数<br>{{afterSaleReception}}</van-grid-item>
+          <van-grid-item @click="showAppointSaleDetails('售后升级接待')">售后升级接待数<br>{{afterSaleUpGradeReception}}</van-grid-item>
+          <van-grid-item @click="showOrderDetails('售后订单')">售后订单数<br>{{afterSaleOrderNum}}</van-grid-item>
           <van-grid-item>售后转换率<br>{{afterSaleProportion}}</van-grid-item>
         </van-grid>
       </van-collapse-item>
@@ -109,6 +109,7 @@
       </van-collapse-item>
     </van-collapse>
 
+    <!-- 店铺业绩弹窗 -->
     <van-popup
         v-model="showDetail"
         closeable
@@ -140,14 +141,14 @@
                     业绩日期：{{ item.createDate }}<br>
                     订单总价：{{ item.orderPrice }}<br>
                     订单店铺：{{ item.city }}<br>
-                    收款日期：{{ item.createDate }}<br>
+                    收款日期：{{ item.createDate }}
                   </van-grid-item>
                   <van-grid-item>
                     收款人：{{ item.payee }}<br>
                     订单编号：{{ item.orderNo }}<br>
                     订单余款：{{ item.orderSpare }}<br>
                     收款项目：{{ item.proceedsName }}<br>
-                    收款方式：{{ item.payment }}<br>
+                    收款方式：{{ item.payment }}
                   </van-grid-item>
                 </van-grid>
               </van-collapse-item>
@@ -155,6 +156,7 @@
           </van-row>
     </van-popup>
 
+    <!-- 营收报告弹窗 -->
     <van-popup
         v-model="showProjectsDetail"
         closeable
@@ -186,13 +188,13 @@
                 订单总价：{{ item.orderPrice }}<br>
                 订单编号：{{ item.orderNo }}<br>
                 订单余款：{{ item.orderSpare }}<br>
-                订单店铺：{{ item.city }}<br>
+                订单店铺：{{ item.city }}
               </van-grid-item>
               <van-grid-item>
                 收款人：{{ item.payee }}<br>
                 收款日期：{{ item.createDate }}<br>
                 收款项目：{{ item.proceedsName }}<br>
-                收款方式：{{ item.payment }}<br>
+                收款方式：{{ item.payment }}
               </van-grid-item>
             </van-grid>
           </van-collapse-item>
@@ -200,6 +202,99 @@
       </van-row>
     </van-popup>
 
+    <!-- 售前接待、售后接待、售后升级接待弹窗 -->
+    <van-popup
+        v-model="showAppointSaleDetailsPopup"
+        closeable
+        position="bottom"
+        close-icon-position="top-right"
+        style="height: 60%; background-color: rgba(255,255,255,0.9) "
+        round>
+      <van-row type="flex" justify="space-around" align="center" style="background-color: rgb(255,255,255); height: 12%">
+        {{ appointSaleDetailsName }}详情
+      </van-row>
+      <br>
+      <van-row type="flex" justify="center">
+        <van-collapse v-model="activeAppointSaleDetail" :border="false" style="width: 90%">
+          <van-collapse-item v-for="(item,index) in appointSaleDetails" :name="index" :key="index">
+            <template #title>
+              <van-grid :border="false" style="text-align: left; font-size: 12px" :column-num="2" :center="false">
+                <van-grid-item>
+                  客户名：{{ item.cusName }}<br>
+                  所属店铺：{{ item.shopName }}
+                </van-grid-item>
+                <van-grid-item>
+                  项目：{{ item.appointName }}<br>
+                  <b>当前状态：{{ item.state }}</b>
+                </van-grid-item>
+              </van-grid>
+            </template>
+            <van-grid :border="false" style="text-align: left; font-size: 12px" :column-num="2" :center="false">
+              <van-grid-item>
+                到店日期：{{ item.arriveDate }}<br>
+                到店时间：{{ item.arriveTime }}<br>
+                预约人：{{ item.inviterName }}<br>
+                礼服师：{{ item.dressName }}<br>
+                客服：{{ item.serviceName }}
+              </van-grid-item>
+              <van-grid-item>
+                预约时状态：{{ item.appState }}<br>
+                渠道：{{ item.source }}<br>
+                结算方式：{{ item.settlementMethod }}<br>
+                <!-- 暂时这个字段没有值 -->
+                <!--渠道对接人：{{ item.channelOppositePerson }}<br>-->
+                意愿程度：{{ item.grade }}<br>
+                手机号：{{ item.phone }}
+              </van-grid-item>
+            </van-grid>
+          </van-collapse-item>
+        </van-collapse>
+      </van-row>
+    </van-popup>
+
+    <!-- 一次订单、二次订单、售后订单弹窗 -->
+    <van-popup
+        v-model="showOrderDetail"
+        closeable
+        position="bottom"
+        close-icon-position="top-right"
+        style="height: 60%; background-color: rgba(255,255,255,0.9) "
+        round>
+      <van-row type="flex" justify="space-around" align="center" style="background-color: rgb(255,255,255); height: 50px">
+        {{ orderDetailName }}详情
+      </van-row>
+      <van-row type="flex" justify="center">
+        <van-collapse v-model="activeOrderDetail" :border="false" style="width: 90%">
+          <van-collapse-item v-for="(item,index) in orderDetail" :name="index" :key="index">
+            <template #title>
+              <van-grid :border="false" style="text-align: left; font-size: 12px" :column-num="2" :center="false">
+                <van-grid-item>
+                  客户名：{{ item.cusName }}<br>
+                  订单店铺：{{ item.shopName }}
+                </van-grid-item>
+                <van-grid-item>
+                  礼服师：{{ item.orderDress }}<br>
+                  <b>确认款式：{{ item.confirmClothes }}</b>
+                </van-grid-item>
+              </van-grid>
+            </template>
+            <van-grid :border="false" style="text-align: left; font-size: 12px" :column-num="2" :center="false">
+              <van-grid-item>
+                订单编号：{{ item.orderNo }}<br>
+                订单总价：{{ item.orderPrice }}<br>
+              </van-grid-item>
+              <van-grid-item>
+                订单余款：{{ item.orderSpare }}<br>
+              </van-grid-item>
+            </van-grid>
+          </van-collapse-item>
+        </van-collapse>
+      </van-row>
+    </van-popup>
+
+    <!-- 一次未订单弹窗 -->
+
+    <!-- 营收弹窗 -->
 
   </div>
 </template>
@@ -219,7 +314,10 @@ export default {
   },
   data() {
     return {
+      showOnceNotOrderDetail: false,
+      showAppointSaleDetailsPopup: false,
       showDetail: false,
+      showOrderDetail: false,
       titleText: "店铺每日业绩",
       loadingList: true,
       tenantCrop: localStorage.getItem("tenantCrop"),
@@ -240,7 +338,7 @@ export default {
       dressName: '',
       dressId: '',
       // 礼服师列表
-      dressArray: [{text: "", id: ""}],
+      dressArray: [{text: "全部礼服师", id: ""}],
       // 是否正在加载礼服师
       dressLoading: false,
       // 是否显示店铺下拉列表
@@ -251,7 +349,7 @@ export default {
       // 是否正在加载店铺
       shopLoading: false,
       // 店铺列表
-      shopArray: [{text: "", id: ""}],
+      shopArray: [{text: "全国店铺", id: ""}],
       activeNames: ['1','2','3','4','5', '6'],
       activeEarningDetail: [],
 
@@ -303,11 +401,50 @@ export default {
       // 是否展开单个详情
       activeProjectsDetail: [],
       // 营收详情
-      projectsDetail: {},
-
+      projectsDetail: [],
+      // 售前/售后 接待详情
+      appointSaleDetails: [],
+      // 接待项目名
+      appointSaleDetailsName: '',
+      // 是否展开单个接待
+      activeAppointSaleDetail: [],
+      // 订单类  名称
+      orderDetailName: '',
+      // 是否展开单个订单
+      activeOrderDetail: [],
+      // 订单类详情
+      orderDetail: [],
+      // 是否展开单个一次未订单
+      activeOnceNotOrderDetail: [],
+      // 一次未订单详情
+      onceNotOrderDetail: [],
     }
   },
   methods: {
+    showOrderDetails(orderType) {
+      if (orderType === "一次订单") {
+        this.queryOrderDetails("售前预约",6)
+      } else if (orderType === "二次订单") {
+        this.queryOrderDetails("售前预约",15)
+      } else if (orderType === "售后订单") {
+        this.queryOrderDetails("售后预约",null)
+      }
+      this.orderDetailName = orderType;
+      this.activeOrderDetail = [];
+      this.showOrderDetail = true;
+    },
+    showAppointSaleDetails(appointSaleType) {
+      if (appointSaleType === "售前接待") {
+        this.queryAppointSaleDetails("售前预约");
+      } else if (appointSaleType === "售后接待") {
+        this.queryAppointSaleDetails("售后预约");
+      } else if (appointSaleType === "售后升级接待") {
+        this.queryAppointSaleDetails("售后升级接待");
+      }
+      this.appointSaleDetailsName = appointSaleType;
+      this.activeAppointSaleDetail = [];
+      this.showAppointSaleDetailsPopup = true;
+    },
     showProjectsDetails(projectName) {
       this.projectsDetail = {}
       this.queryProjectsDetails(projectName);
@@ -438,19 +575,52 @@ export default {
       }).then(response => {
         this.projectsDetail = response.data.data;
       })
-    }
-  },
-  computed: {
-    revenueReports: function () {
-      return this.proceedsAnalysis.filter((item) => {
-        return item.projectName !== '押金'
+    },
+    queryAppointSaleDetails(appointSaleType) {
+      this.$axios({
+        method: "GET",
+        url: "/shopReports/queryAppointSaleDetails",
+        params: {
+          startDate: this.startDate,
+          endDate: this.endDate,
+          tenantCrop: this.tenantCrop,
+          shopId: this.shopId,
+          dressId: this.dressId,
+          appointSaleType: appointSaleType
+        }
+      }).then(response => {
+        this.appointSaleDetails = response.data.data;
       })
     },
-    depositReports: function () {
-      return this.proceedsAnalysis.filter((item) => {
-        return item.projectName === '押金'
+    queryOrderDetails(type, appState) {
+      this.$axios({
+        method: "GET",
+        url: "/shopReports/queryOrderDetails",
+        params: {
+          startDate: this.startDate,
+          endDate: this.endDate,
+          tenantCrop: this.tenantCrop,
+          shopId: this.shopId,
+          dressId: this.dressId,
+          type: type,
+          appState: appState
+        }
+      }).then(response => {
+        this.orderDetail = response.data.data;
       })
-    }
+    },
+  },
+  computed: {
+    // revenueReports: function () {
+    //   return this.proceedsAnalysis.filter((item) => {
+    //     return item.projectName !== '押金'
+    //   })
+    // },
+    // depositReports: function () {
+    //   return this.proceedsAnalysis.filter((item) => {
+    //     return item.projectName === '押金'
+    //   })
+    // }
   }
 
 }
