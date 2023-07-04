7e38ce48c12d8022444dbe351e2838a832a76212
Aaron <827052236@qq.com>
2023-07-04 17:09:52 +0800

    1.余额展示完成

--- src/pages/user/account_info.vue
+++ src/pages/user/account_info.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
-    <baseNavBar title="账户信息" />
+    <van-sticky>
+      <baseNavBar title="账户信息"/>
+    </van-sticky>
     <div class="card">
+      <div style="text-align: center;padding-top: 5%">
+        <p style="font-size: 40px;font-weight: bolder">{{mathUtils.add(advanceCharge,cashPledge)}}</p>
+        <p style="font-size: 20px;">总余额(元)</p>
+      </div>
+      <van-row style="text-align: center;margin-top: 7%">
+        <van-col :span="12">
+          <div >
+            <p style="font-size: 25px;font-weight: bolder">{{advanceCharge}}</p>
+            <p style="font-size: 15px;">预付款余额(元)</p>
+          </div>
+        </van-col>
+        <van-col :span="12">
+          <div>
+            <p style="font-size: 25px;font-weight: bolder">{{cashPledge}}</p>
+            <p style="font-size: 15px;">押金余额(元)</p>
+          </div>
+        </van-col>
+      </van-row>
 
     </div>
   </div>
@@ -10,13 +30,22 @@
 <script>
 
 import baseNavBar from "@/components/nav-bar/base-nav-bar"
+import mathUtils from "@/common/js/utils/math-utils";
 
 export default {
   name: "account_info",
+  computed: {
+    mathUtils() {
+      return mathUtils
+    }
+  },
   data() {
     return {
-      tenantVO:{},
+      tenantVO: {},
       tenantCrop: localStorage.getItem("tenantCrop"),
+      advanceCharge:0,
+      cashPledge:0,
+
 
     }
   },
@@ -24,11 +53,10 @@ export default {
     baseNavBar
   },
   created() {
-
+    this.queryAdvanceCharge();
+    this.queryTenantBill();
   },
-  watch:{
-
-  },
+  watch: {},
   methods: {
     //查询公司信息
     queryAdvanceCharge() {
@@ -39,9 +67,24 @@ export default {
           tenantCrop: this.tenantCrop
         }
       }).then(response => {
+        this.tenantVO=response.data.data;
         this.advanceCharge = response.data.data.advanceCharge;
-        this.styleDiscountRatio = response.data.data.styleDiscountRatio;
-        this.getCountPrice();
+        this.cashPledge = response.data.data.cashPledge;
+
+      })
+    },
+
+    //查询公司信息
+    queryTenantBill() {
+      this.$axios({
+        method: "GET",
+        url: "/tenantBill/queryList",
+        params: {
+          tenantCrop: this.tenantCrop
+        }
+      }).then(response => {
+        console.log(response)
+
       })
     },
 
@@ -51,12 +94,19 @@ export default {
 </script>
 
 <style scoped>
-.card{
+.card {
   margin: 3% auto 0 auto;
-  background-color: #1AA090;
-  height: 200px;
+  background-color: #6391d2;
+  height: 180px;
   width: 90%;
-
+  color: #ffffff;
   border-radius: 10px;
+}
+
+p{
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
 }
 </style>
