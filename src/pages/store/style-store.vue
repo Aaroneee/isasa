e709ea8825d204a607fbbbca71bfd70352e9069e
Aaron <827052236@qq.com>
2023-08-14 14:38:42 +0800

    1.添加款式商城首页品牌筛选

--- src/pages/store/style-store.vue
+++ src/pages/store/style-store.vue
@@ -2,10 +2,13 @@
   <div>
     <van-sticky>
       <BaseNavBar title="款式商城"/>
+      <van-dropdown-menu>
+        <van-dropdown-item :title="brandText" v-model="brand" @change="brandChange"  :options="styleBrandArray"/>
+      </van-dropdown-menu>
     </van-sticky>
     <div>
       <van-collapse v-model="activeNames">
-        <van-collapse-item :title="item.brandName" :name="index"  v-for="(item,index) in brandList" :key="item.id">
+        <van-collapse-item :title="item.brandName" :name="index"  v-for="(item,index) in brandStyleList" :key="item.id">
           <van-row gutter="5">
             <van-col span="12" v-for="(chilItem,chilIndex) in item.storeSeriesVOS" :key="chilItem.id" style="text-align: center">
               <div class="card" @click="toStyleList(chilItem.id)">
@@ -53,27 +56,48 @@ export default {
   data() {
     return {
       activeNames: [0],
-      brandList: [],
+      brandStyleList: [],
+
+      brand:"",
+      brandText:"",
+      styleBrandArray: [{text: "款式品牌", value: ""}],
       tenantCrop: localStorage.getItem("tenantCrop"),
     }
   },
   created() {
-    this.queryStoreSeriesList()
+    this.queryStoreBrandList()
+    this.queryStoreBrand()
   },
   components: {
     BaseNavBar
   },
   methods: {
-    queryStoreSeriesList: function () {
+    queryStoreBrandList: function () {
       this.$axios({
         method: "GET",
         url: "/storeBrand/queryList",
         params: {
+          id:this.brand,
           tenantCrop: this.tenantCrop,
         }
       }).then(response => {
-        this.brandList = response.data.data;
+        this.brandStyleList = response.data.data;
       })
+    },
+    //查询品牌库
+    queryStoreBrand: function () {
+      this.$selectUtils.queryStoreBrandIds(this.$selectUtils.DropDownMenu).then((response) => {
+        this.styleBrandArray.push(...JSON.parse(response.data.data))
+      })
+    },
+    //切换品牌
+    brandChange: function () {
+      this.dataClear()
+      this.queryStoreBrandList();
+    },
+    dataClear() {
+      this.page = 1
+      this.styleList = []
     },
     toStyleList(seriesId){
       this.$router.push({name: 'styleStoreStyleList', query: {id: seriesId}})
