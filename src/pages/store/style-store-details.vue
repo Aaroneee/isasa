8d138f782bb6f727ab166425ef1396f03b2e89a4
Aaron <827052236@qq.com>
2023-09-20 16:00:24 +0800

    1.贸易婚纱尺寸默认为M

--- src/pages/store/style-store-details.vue
+++ src/pages/store/style-store-details.vue
@@ -191,7 +191,7 @@ export default {
       previewImages: [],
       imgTypeName: "",
 
-      clothesSize:"S",
+      clothesSize:"M",
       clothesSizeShow:false,
       clothesSizePicker:false,
       clothesSizeArray:['S', 'M', 'L', 'F'],
